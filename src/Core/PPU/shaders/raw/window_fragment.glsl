e12f67a319443788328a4e2a11a090b95690b218
gp.coder <gp.coder@163.com>
2025-12-08 22:44:52 +0800

    1.修复窗口判断的bug

--- src/Core/PPU/shaders/raw/window_fragment.glsl
+++ src/Core/PPU/shaders/raw/window_fragment.glsl
@@ -13,75 +13,106 @@ uint readVRAM32(uint address);
 uint readIOreg(uint address);
 vec4 readPALentry(uint index);
 
-void main() {
-    uint DISPCNT = readIOreg(++DISPCNT++);
 
-    if ((DISPCNT & 0xe000u) == 0u) {
-        // windows are disabled, enable all windows
-        // we should have caught this before rendering, but eh, I guess we'll check again...
+const uint DISPCNT_WIN0_EN = 1u << 13u; 
+const uint DISPCNT_WIN1_EN = 1u << 14u; 
+const uint DISPCNT_OBJWIN_EN = 1u << 15u;
+
+void main() {
+    
+    uint DISPCNT = readIOreg(++DISPCNT++);
+    
+    if ((DISPCNT & (DISPCNT_WIN0_EN | DISPCNT_WIN1_EN | DISPCNT_OBJWIN_EN)) == 0u) {
         FragColor.x = 0x3fu;
         gl_FragDepth = 1.0;
         return;
     }
+    
+    
+    uint x = clamp(uint(screenCoord.x), 0u, ++VISIBLE_SCREEN_WIDTH++ );
+    uint y = clamp(uint(screenCoord.y), 0u, ++VISIBLE_SCREEN_HEIGHT++);
 
-    uint x = uint(screenCoord.x);
-    uint y = uint(screenCoord.y);
+    
+    if ((DISPCNT & DISPCNT_WIN0_EN) != 0u) {
+       
+        uint WIN0H = readIOreg(++WIN0H++);
+        uint WIN0V = readIOreg(++WIN0V++);
+        
+        
+        uint X1 = clamp(WIN0H >> 8u, 0u, ++VISIBLE_SCREEN_WIDTH++ );
+        uint X2 = clamp(WIN0H & 0xffu, 0u, ++VISIBLE_SCREEN_WIDTH++ );
+        uint Y1 = clamp(WIN0V >> 8u, 0u, ++VISIBLE_SCREEN_HEIGHT++);
+        uint Y2 = clamp(WIN0V & 0xffu, 0u, ++VISIBLE_SCREEN_HEIGHT++);
 
-    // window 0 has higher priority
-    for (uint window = 0u; window < 2u; window++) {
-        if ((DISPCNT & (++DisplayWin0++ << window)) == 0u) {
-            // window disabled
-            continue;
-        }
-
-        uint WINH = readIOreg(++WIN0H++ + 2u * window);
-        uint WINV = readIOreg(++WIN0V++ + 2u * window);
-        uint WININ = (readIOreg(++WININ++) >> (window * 8u)) & 0x3fu;
-
-        uint X1 = WINH >> 8;
-        uint X2 = WINH & 0xffu;
-        if (X2 > ++VISIBLE_SCREEN_WIDTH++) {
-            X2 = ++VISIBLE_SCREEN_WIDTH++;
-        }
-
-        uint Y1 = WINV >> 8;
-        uint Y2 = WINV & 0xffu;
-
+      
+        bool inVerticalRange = false;
         if (Y1 <= Y2) {
-            // no vert wrap and out of bounds, continue
-            if (y < Y1 || y > Y2) {
-                continue;
-            }
-        }
-        else {
-            // vert wrap and "in bounds":
-            if ((y < Y1) && (y > Y2)) {
-                continue;
-            }
+            
+            inVerticalRange = (y >= Y1) && (y < Y2);
+        } else {
+           
+            inVerticalRange = (y > Y1) || (y <= Y2);
         }
 
+       
+        bool inHorizontalRange = false;
         if (X1 <= X2) {
-            // no hor wrap
-            if (x >= X1 && x < X2) {
-                // pixel in WININ
-                FragColor.x = WININ;
-                gl_FragDepth = 0.0;
-                return;
-            }
+            
+            inHorizontalRange = (x >= X1) && (x < X2);
+        } else {
+            
+            inHorizontalRange = (x > X1) || (x <= X2);
         }
-        else {
-            // hor wrap
-            if (x < X2 || x >= X1) {
-                // pixel in WININ
-                FragColor.x = WININ;
-                gl_FragDepth = 0.0;
-                return;
-            }
+
+       
+        if (inVerticalRange && inHorizontalRange) {
+            uint WININ = readIOreg(++WININ++);             
+            uint WIN0_IN = (WININ >> 0u) & 0x3fu;
+            FragColor.x = WIN0_IN;
+            gl_FragDepth = 0.0; 
+            return;
         }
     }
 
-    FragColor.x = readIOreg(++WINOUT++) & 0x3fu;  // WINOUT
-    gl_FragDepth = 1.0;
-}
+    if ((DISPCNT & DISPCNT_WIN1_EN) != 0u) {
+       
+        uint WIN1H = readIOreg(++WIN0H++ + 2u);
+        uint WIN1V = readIOreg(++WIN0V++ + 2u);
+        
+        
+        uint X1 = clamp(WIN1H >> 8u, 0u, ++VISIBLE_SCREEN_WIDTH++ );
+        uint X2 = clamp(WIN1H & 0xffu, 0u, ++VISIBLE_SCREEN_WIDTH++ );
+        uint Y1 = clamp(WIN1V >> 8u, 0u, ++VISIBLE_SCREEN_HEIGHT++ );
+        uint Y2 = clamp(WIN1V & 0xffu, 0u, ++VISIBLE_SCREEN_HEIGHT++ );
 
+       
+        bool inVerticalRange = false;
+        if (Y1 <= Y2) {
+            inVerticalRange = (y >= Y1) && (y < Y2);
+        } else {
+            inVerticalRange = (y > Y1) || (y <= Y2);
+        }
+
+       
+        bool inHorizontalRange = false;
+        if (X1 <= X2) {
+            inHorizontalRange = (x >= X1) && (x < X2);
+        } else {
+            inHorizontalRange = (x > X1) || (x <= X2);
+        }
+
+        
+        if (inVerticalRange && inHorizontalRange) {
+            uint WININ = readIOreg(++WININ++);
+           
+            uint WIN1_IN = (WININ >> 8u) & 0x3fu; 
+            FragColor.x = WIN1_IN;
+            gl_FragDepth = 0.0;
+            return;
+        }
+    }
+    uint WINOUT = readIOreg(++WINOUT++) & 0x3fu;
+    FragColor.x = WINOUT;
+    gl_FragDepth = 1.0; 
+}
 // END WindowFragmentShaderSource
